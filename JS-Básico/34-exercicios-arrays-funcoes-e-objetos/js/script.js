987dd8cc0a62818b792dc7cb5ee921808e1c57b7
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-03-16 19:45:59 -0300

    Criei uma array com objetos toda vez que o form for enviado
    Foi criado nesse exercicio um form, onde toda vez que ele for enviado fosse criado um array contendo objeto com os dados desse form.

---
+++ JS-Básico/34-exercicios-arrays-funcoes-e-objetos/js/script.js
@@ -0,0 +1,32 @@
+function meuEscopo() {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+    const pessoas = []; 
+
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        pessoas.push({
+            'nome': nome.value, 
+            'sobrenome': sobrenome.value, 
+            'peso': peso.value, 
+            'altura': altura.value
+        });
+        
+        console.log(pessoas);
+        // console.log(pessoas[0].nome) // Eduardo
+
+        resultado.innerHTML += `<p>O nome é: ${nome.value}</p<br />`;
+        resultado.innerHTML += `<p>O sobrenome é: ${sobrenome.value}</p<br />`;
+        resultado.innerHTML += `<p>O peso é: ${peso.value} Kg</p<br />`;
+        resultado.innerHTML += `<p>A altura é: ${altura.value}m</p<br />`;
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
+}
+
+meuEscopo()
