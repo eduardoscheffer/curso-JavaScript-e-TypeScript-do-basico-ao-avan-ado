3467ae52913c11872a09463b2e7da356356dc3e0
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-04-29 10:28:18 -0300

    studied webpack config and started import/export modules

---
+++ es6-modules/modules-import-export/src/index.js
@@ -0,0 +1,3 @@
+import { nome } from './modulo1'; // importa a constante nome do módulo1.js
+
+console.log(nome);
---
+++ es6-modules/modules-import-export/src/modulo1.js
@@ -0,0 +1,7 @@
+const nome = 'Eduardo';
+const sobrenome = 'Miranda';
+const idade = 25;
+
+const soma = (x, y) => x + y;
+
+export { nome } // exporta a constante nome do módulo1.js
