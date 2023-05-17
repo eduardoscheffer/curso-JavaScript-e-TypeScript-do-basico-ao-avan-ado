88290067c0aa6f7a31ff5b6278a38878773a713d
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-05-17 18:39:50 -0300

    created first server using express

---
+++ Node-Npm-Express-MongoDB/Express/Introducao/server.js
@@ -0,0 +1,29 @@
+const express = require('express'); // importa o express
+const app = express(); // cria uma instancia do express na variavel app
+
+
+//          POST   GET    PUT    DELETE
+// CRUD -> CREATE. READ, UPDATE, DELETE
+
+// http://meusite.com/ -> GET == entregue a página
+
+app.get('/', (req, res) => { // metodo get que entrega um formulario quando o cliente acessa a rota '/'
+    res.send(`
+    <form action ="/" method="POST">
+    Nome: <input type="text" name="nome">
+    <button>Enviar</button>
+    </form>
+    `); // envia pra rota o formulário
+});
+
+app.post('/', (req, res) => {
+    res.send('Formulário recebido.')
+})
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado por entrar em contato.');
+})
+
+app.listen(3000, () => {
+    console.log('Servidor executando na porta 3000');
+});
