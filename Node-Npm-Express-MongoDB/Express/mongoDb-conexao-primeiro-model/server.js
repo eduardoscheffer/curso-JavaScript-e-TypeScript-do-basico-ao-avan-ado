e035892b56af43eb4c45a0860a3be5d9650133d5
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-05-26 13:15:37 -0300

    update server.js. sessions with connect-mongo and express-sessions

--- Node-Npm-Express-MongoDB/Express/mongoDb-conexao-primeiro-model/server.js
+++ Node-Npm-Express-MongoDB/Express/mongoDb-conexao-primeiro-model/server.js
@@ -10,15 +10,34 @@ mongoose.connect(process.env.CONNECTIONSTRING) // retorna uma promise
         console.log('Conectei a base de dados');
         app.emit('pronto'); // emite um sinal pro app quando a db estiver conectada
     })
+    .catch(e => console.log(e));
 
-
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
 const routes = require('./routes'); // importa as rotas
 const path = require('path'); // importa o uso do path
 const { middleWareGlobal } = require('./src/middlewares/middleware'); // importa o(s) middleware(s) globais
 
 app.use(express.urlencoded( {extended: true} )); // diz pro express pra tratar os dados do req.body
-
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public'))); // configura o express pra usar conteudo estatico
+
+// opções da sessions
+const sessionOptions = session({
+    secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true
+    }
+  }); 
+
+app.use(sessionOptions); // diz pra aplicação usar sessions
+app.use(flash());
+
 
 app.set('views', path.resolve(__dirname, 'src', 'views')); // configura o express pra usar os views
 app.set('view engine', 'ejs'); // seta a engine do 'html' pra ser a ejs
