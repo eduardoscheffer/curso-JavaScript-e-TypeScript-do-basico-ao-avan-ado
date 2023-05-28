437ce7e01240cf08990f4d7dd4607c2dbea13e7c
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-05-28 19:26:44 -0300

    updated UserSchema and mongoose queries

--- Node-Npm-Express-MongoDB/Express/mongodb-mongoose-basics/User.js
+++ Node-Npm-Express-MongoDB/Express/mongodb-mongoose-basics/User.js
@@ -1,10 +1,32 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({ // comando pra criar um novo Schema
-    name: String,
-    age: Number
+    name: String, // key: typeof the key
+    age: {
+        type: Number,
+        min: 1,
+        max: 120
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+
+    },
+    createdAt: {
+        type: Date,
+        default: () => Date.now(),
+        
+    },
+    updatedAt: Date,
+    // bestFriend: mongoose.SchemaType.ObjectId,
+    hobbies: [String],
+    address: {
+        street: String,
+        city: String, 
+    }
 })
 
 // feito o Schema, se cria agr o Model pra esse Schema:
-//        Nome da collection
+//                      Nome da collection
 module.exports = mongoose.model('User', UserSchema);
--- Node-Npm-Express-MongoDB/Express/mongodb-mongoose-basics/server.js
+++ Node-Npm-Express-MongoDB/Express/mongodb-mongoose-basics/server.js
@@ -17,8 +17,41 @@ async function connectDB () {
 run()
 async function run() {
     
-    const user = new User({ name: 'Mariana', age: 26 }); // cria o novo User e salva localmente na variavel user
+    try {
 
-    await user.save(); // salva na database
-    console.log('Saved');
+        // const user = new User({ name: 'Mariana', age: 26 }); // cria o novo User e salva localmente na variavel user
+        // await user.save(); // salva na database
+        /*
+        ---- UTILIZANDO O MESMO MÉTODO DE CIMA MAS o metodo .create() cria e já salva automaticamente, não sendo necessário fazer o user.save():
+        const user = User.create({
+            name: 'Mariana',
+            age: 26,
+            email: teste@teste.com.br,
+            hobbies: ["Musculação", "Corrida"],
+            address: {street: "Main St", city: "Tc"}
+        })
+        */
+
+        // console.log('Saved'); // printa a mensagem assim que o documento for salvo na db
+        // const findMari = await User.where('name').equals('Mariana');
+        // console.log(findMari);
+        /*
+        [
+            {
+                hobbies: [],
+                _id: new ObjectId("647273c76168018c1e212145"),
+                name: 'Mariana',
+                age: 26,
+                __v: 0,
+                createdAt: 2023-05-28T22:13:33.867Z
+            }
+        ]
+        */
+
+        // const everythinInMongo = await User.find(); // tras todos os documentos que tem na collection users(User)
+        // console.log(everythinInMongo);
+
+    } catch (e) {
+        console.log(e.message);
+    }
 }
